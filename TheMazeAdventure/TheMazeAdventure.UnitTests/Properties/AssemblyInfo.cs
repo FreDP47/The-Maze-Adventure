e5ff497705455b9361314e0fd44b60e74b092ca0
Pratyush Maharana <PMAHARAN@volvocars.com>
2021-03-20 11:28:22 +0100

    Added different layers and uni testing projects

---
+++ TheMazeAdventure/TheMazeAdventure.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TheMazeAdventure.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TheMazeAdventure.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("18102ca9-c129-4da3-9c6a-da512b312e67")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TheMazeAdventure/TheMazeAdventure.UnitTests/TheMazeAdventureServicesTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TheMazeAdventure.UnitTests
+{
+    [TestClass]
+    public class TheMazeAdventureServicesTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
