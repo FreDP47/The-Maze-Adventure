017e68c20245be334e9796012e95bd8acf4ba948
Pratyush Maharana <PMAHARAN@volvocars.com>
2021-03-20 10:48:01 +0100

    Added IoC container and swashbuckle for swagger generation

---
+++ TheMazeAdventure/TheMazeAdventure.API/App_Start/UnityResolver.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http.Dependencies;
+using Unity;
+
+namespace TheMazeAdventure.API
+{
+    public sealed class UnityResolver : IDependencyResolver
+    {
+        private readonly IUnityContainer _container;
+
+        public UnityResolver(IUnityContainer container)
+        {
+            this._container = container ?? throw new ArgumentNullException(nameof(container));
+        }
+
+        public object GetService(Type serviceType)
+        {
+            try
+            {
+                return _container.Resolve(serviceType);
+            }
+            catch (ResolutionFailedException exception)
+            {
+                throw new InvalidOperationException(
+                    $"Unable to resolve service for type {serviceType}.",
+                    exception);
+            }
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            try
+            {
+                return _container.ResolveAll(serviceType);
+            }
+            catch (ResolutionFailedException exception)
+            {
+                throw new InvalidOperationException(
+                    $"Unable to resolve service for type {serviceType}.",
+                    exception);
+            }
+        }
+
+        public IDependencyScope BeginScope()
+        {
+            var child = _container.CreateChildContainer();
+            return new UnityResolver(child);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            _container.Dispose();
+        }
+    }
+}
+
--- TheMazeAdventure/TheMazeAdventure.API/App_Start/WebApiConfig.cs
+++ TheMazeAdventure/TheMazeAdventure.API/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using Unity;
 
 namespace TheMazeAdventure.API
 {
@@ -10,6 +11,8 @@ namespace TheMazeAdventure.API
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            var container = new UnityContainer();
+            config.DependencyResolver = new UnityResolver(container);
 
             // Web API routes
             config.MapHttpAttributeRoutes();
