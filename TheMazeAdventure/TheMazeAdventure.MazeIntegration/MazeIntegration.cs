66b049e1eec72cc68089dab9147588cf8ad476df
Pratyush Maharana <PMAHARAN@volvocars.com>
2021-03-21 17:07:19 +0100

    Added project for implementation

---
+++ TheMazeAdventure/TheMazeAdventure.MazeIntegration/MazeIntegration.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AtlasCopco.Integration.Maze;
+
+namespace TheMazeAdventure.MazeIntegration
+{
+    public class MazeIntegration: IMazeIntegration
+    {
+
+        public void BuildMaze(int size)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetEntranceRoom()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int? GetRoom(int roomId, char direction)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetDescription(int roomId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool HasTreasure(int roomId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CausesInjury(int roomId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
